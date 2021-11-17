080b59075f29c6b8b124478c60ec538103a12a51
FerranHPV3 <ferranhpv3@gmail.com>
2021-11-17 18:45:11 +0100

    Modify customer method implemented

--- src/main/java/com/example/demo/customer/CustomerEndpoint.java
+++ src/main/java/com/example/demo/customer/CustomerEndpoint.java
@@ -6,10 +6,12 @@ import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
+import io.spring.guides.gs_producing_web_service.CreateCustomerRequest;
+import io.spring.guides.gs_producing_web_service.Customer;
+import io.spring.guides.gs_producing_web_service.CustomerResponse;
 import io.spring.guides.gs_producing_web_service.DeleteCustomerRequest;
-import io.spring.guides.gs_producing_web_service.DeleteCustomerResponse;
 import io.spring.guides.gs_producing_web_service.GetCustomerRequest;
-import io.spring.guides.gs_producing_web_service.GetCustomerResponse;
+import io.spring.guides.gs_producing_web_service.ModifyCustomerRequest;
 
 
 @Endpoint
@@ -24,19 +26,37 @@ public class CustomerEndpoint {
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCustomerRequest")
     @ResponsePayload
-    public GetCustomerResponse getCustomer(@RequestPayload GetCustomerRequest request) {
+    public CustomerResponse getCustomer(@RequestPayload GetCustomerRequest request) {
         System.out.println("CustomerEndpoint.getCustomer()");
-        GetCustomerResponse response = new GetCustomerResponse();
+        CustomerResponse response = new CustomerResponse();
         response.setCustomer(customerRepository.findCustomerByID(request.getId()));
         return response;
     }
 
     @PayloadRoot(namespace = NAMESPACE_URI, localPart = "deleteCustomerRequest")
     @ResponsePayload
-    public DeleteCustomerResponse deleteCustomer(@RequestPayload DeleteCustomerRequest request) {
+    public CustomerResponse deleteCustomer(@RequestPayload DeleteCustomerRequest request) {
         System.out.println("CustomerEndpoint.deleteCustomer()");
-        DeleteCustomerResponse response = new DeleteCustomerResponse();
+        CustomerResponse response = new CustomerResponse();
         response.setCustomer(customerRepository.deleteCustomerByID(request.getId()));
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "createCustomerRequest")
+    @ResponsePayload
+    public CustomerResponse createCustomer(@RequestPayload CreateCustomerRequest request) {
+        System.out.println("CustomerEndpoint.createCustomer()");
+        CustomerResponse response = new CustomerResponse();
+        response.setCustomer(customerRepository.createCustomer(request.getName(),request.getFirstName(),request.getLastName(),request.getEmail()));
+        return response;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "modifyCustomerRequest")
+    @ResponsePayload
+    public CustomerResponse modifyCustomer(@RequestPayload ModifyCustomerRequest request) {
+        System.out.println("CustomerEndpoint.modifyCustomer()");
+        CustomerResponse response = new CustomerResponse();
+        response.setCustomer(customerRepository.modifyCustomer(request));
         return response;
     }
 }
--- src/main/java/com/example/demo/customer/CustomerRepository.java
+++ src/main/java/com/example/demo/customer/CustomerRepository.java
@@ -7,11 +7,13 @@ import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 import io.spring.guides.gs_producing_web_service.Customer;
+import io.spring.guides.gs_producing_web_service.ModifyCustomerRequest;
 
 
 @Component
 public class CustomerRepository {
     private static final Map<Long, Customer> customers = new HashMap<>();
+    private static int id = 3;
 
     @PostConstruct
     public void initData() {
@@ -45,6 +47,20 @@ public class CustomerRepository {
         return customers.get(name);
     }*/
 
+    public Customer createCustomer(String name, String firstName, String lastName, String email) {
+        //Assert.notNull(id, "The customer's id must not be null");
+        this.id = this.id + 1;
+        Customer customer = new Customer();
+        System.out.println(this.id);
+        customer.setId(this.id);
+        customer.setName(name);
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        customer.setEmail(email);
+        customers.put(customer.getId(), customer);
+        return customers.get(customer.getId());
+    }
+
     public Customer findCustomerByID(long id) {
         Assert.notNull(id, "The customer's id must not be null");
         return customers.get(id);
@@ -53,5 +69,21 @@ public class CustomerRepository {
     public Customer deleteCustomerByID(long id) {
         Assert.notNull(id, "The customer's id must not be null");
         return customers.remove(id);
+    }
+
+    public Customer modifyCustomer(ModifyCustomerRequest request){
+        Customer myCustomer = customers.get(request.getId());
+        if(!request.getName().isEmpty()){
+            myCustomer.setName(request.getName());
+        }else if(!request.getFirstName().isEmpty()){
+            myCustomer.setFirstName(request.getFirstName());
+        }if(!request.getLastName().isEmpty()){
+            myCustomer.setLastName(request.getLastName());
+        }if(!request.getEmail().isEmpty()){
+            myCustomer.setEmail(request.getEmail());
+        }
+        customers.remove(request.getId());
+        customers.put(request.getId(), myCustomer);
+        return customers.get(request.getId());
     }
 }
